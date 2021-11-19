9300542b19c2fe79e08beb2e9a3f24bfc4dff9e0
hassan khalil <hkhalil@student.1337.ma>
2021-11-19 23:41:50 +0100

    fix hex categories

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -2,11 +2,11 @@
 
 void	ft_print_hex(int n, char* base, int *l)
 {
-	unsigned int	nb;
+	unsigned long	nb;
 
 	nb = n;
 	if (nb >= 16)
-		ft_print_hex((nb/16), "0123456789abcdef", l);
+		ft_print_hex((nb/16), base, l);
 	write(1, &base[nb % 16], 1);
 	(*l)++;
 }
