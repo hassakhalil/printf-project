a045fbfce2f93cb420913246a2e69b2847b28b58
hassan khalil <hkhalil@student.1337.ma>
2021-11-20 02:24:59 +0100

    project finished

--- ft_print_base.c
+++ ft_print_base.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_base.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/20 02:20:23 by hkhalil           #+#    #+#             */
+/*   Updated: 2021/11/20 02:22:02 by hkhalil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void	ft_print_base(int n, char* base,int size, int *l)
+void	ft_print_base(int n, char *base, int size, int *l)
 {
 	unsigned long	nb;
 
 	nb = n;
 	if (nb >= (unsigned long)size)
-		ft_print_base((nb/size), base, size, l);
+		ft_print_base((nb / size), base, size, l);
 	ft_putchar(base[nb % size], l);
 }
