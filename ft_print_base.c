5670859cccce2a1c62cbfcd4eaf0895cc2091d86
hassan khalil <hkhalil@student.1337.ma>
2021-11-20 04:25:44 +0100

    project finished

--- ft_print_base.c
+++ ft_print_base.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 03:34:02 by hkhalil           #+#    #+#             */
-/*   Updated: 2021/11/20 04:18:50 by hkhalil          ###   ########.fr       */
+/*   Updated: 2021/11/20 04:25:31 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	ft_print_base(unsigned long nb, char *base, int size, int *l)
 {
-	//unsigned int	nb;
-
-	//nb = n;
 	if (nb >= (unsigned long)size)
 		ft_print_base((nb / size), base, size, l);
 	ft_putchar(base[nb % size], l);
